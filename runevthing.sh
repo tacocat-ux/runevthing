06de55fe163ce94a4bc0e9bf75eddb94d9b22e67
tacocat-ux <83519757+tacocat-ux@users.noreply.github.com>
2021-05-01 22:11:25 -0600

    Create runevthing.sh

---
+++ runevthing.sh
@@ -0,0 +1,27 @@
+apt-get update && apt-get upgrade -y
+ 
+sudo apt install tree -y
+
+apt-get install -y
+
+sudo apt-get install wget -y
+
+sudo apt-get install proxychains-ng -y
+
+sudo apt-get install nmap -y
+
+sudo apt-get install netstat -y
+
+sudo apt-get install netcat -y
+
+sudo apt install wireshark -y
+
+sudo apt-get install tor
+
+apt-get install python -y && apt-get install python2 -y & apt-get install python3 -y
+
+apt-get install fullupgrade -y
+
+dist-upgrade apt-get clean -y
+
+apt-get install php -y && apt-get install pip -y
